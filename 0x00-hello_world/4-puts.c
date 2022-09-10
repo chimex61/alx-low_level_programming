34465c3d1166ae0cc986d86b1375dc446fc822e1
Francis John <chimex61@gmail.com>
2022-09-10 08:44:49 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,7 @@
  * File: 4-puts.c
  * Author: Francis John 
  */
-
 #include <stdio.h>
-
 /**
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
