1c28924c813526709002fa1adb156e6527d66305
Francis John <chimex61@gmail.com>
2022-09-12 17:43:42 +0100

    Update and rename 10-print_comb2.c to 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - Prints the numbers from 00 to 99
+* main - Prints combination of numbers
 *
 * Return: Always (Success)
 */
@@ -18,7 +18,7 @@ int main(void)
 			putchar(c);
 			putchar(i);
 			
-			if ((c != '9') || (c == '9' && i != '9'))
+			if ((c != '8') || (c == '8' && i != '9'))
 			{
 				putchar(',');
 				putchar(' ');
