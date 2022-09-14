da3ebf89613ec0049553904f7cdc12a2980cdf37
Francis John <chimex61@gmail.com>
2022-09-14 07:16:12 -0700

    first

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,21 @@
 int main(void)
 {
 	int c, i;
-	
-	c = i = '0';
-	
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
 			putchar(c);
 			putchar(i);
-			
-			if ((c != '8') || (c == '8' && i != '9'))
+
+			if (c != '8' || (c == '8' && i != '9'))
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	
-	putchar('\n');
-	return (0);
-}
+	
+[2~[2~OF
+OHOQOP]
